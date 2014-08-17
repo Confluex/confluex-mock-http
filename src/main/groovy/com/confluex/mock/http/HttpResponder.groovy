2447a44d28441c9a967a02f913443dfa84907bed
Chad Gorshing <gorshing@gmail.com>
2014-08-17 15:55:42 -0500

    Added a method to help with gzip compressed data.

--- src/main/groovy/com/confluex/mock/http/HttpResponder.groovy
+++ src/main/groovy/com/confluex/mock/http/HttpResponder.groovy
@@ -6,13 +6,15 @@ import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 
 import javax.servlet.http.HttpServletResponse
+import java.util.zip.GZIPOutputStream
 
 import static javax.servlet.http.HttpServletResponse.*
 
 @Slf4j
 class HttpResponder {
+    boolean asGzipped
     int status = SC_OK
-    Resource body = new ByteArrayResource("".bytes)
+    Resource body = new ByteArrayResource(''.bytes)
     Closure bodyClosure
 
     Map<String, String> headers = [:]
@@ -24,6 +26,16 @@ class HttpResponder {
         headers.each { k, v ->
             response.addHeader(k, v)
         }
+
+        if (asGzipped) {
+            def targetStream = new ByteArrayOutputStream()
+            def zipStream = new GZIPOutputStream(targetStream)
+            zipStream << body
+            zipStream.close()
+
+            body = targetStream.toByteArray()
+        }
+
         response.outputStream << body
     }
 }
@@ -53,6 +65,12 @@ class HttpResponderBuilder {
 
     HttpResponderBuilder withHeader(String name, String value) {
         responder.headers[name] = value
+        this
+    }
+
+    HttpResponderBuilder asGzipped() {
+        responder.headers['Content-Encoding'] = 'gzip'
+        responder.asGzipped = true
         this
     }
 }
